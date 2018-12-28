eec16cffdfd574586dfd6ee851a6b7db1e559016
Arabyan <grigorarabyan@yahoo.com>
2018-12-29 03:26:17 +0400

    Created Homepage View & then Voting

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from . import models
+from .models import  Post
 
 @login_required
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['url']:
-            post = models.Post()
+            post = Post()
             post.title = request.POST['title']
-            post.url = request.POST['url']
+            if request.POST['url'].startswith('http://') or  request.POST['url'].startswith('https://'):
+                post.url = request.POST['url']
+            else:
+                post.url = 'http://' + request.POST['url']
             post.pub_date = timezone.datetime.now()
             post.author = request.user
             post.save()
-            return render(request, 'posts/home.html')
+            return redirect('home')
         else:
             return render(request, 'posts/create.html', {'error':'ERROR: you must include a title and URL  to create a post'})
 
@@ -22,4 +25,21 @@ def create(request):
 
 
 def home(request):
-    return render(request, 'posts/home.html')
+    posts = Post.objects.order_by('votes_total')
+    return render(request, 'posts/home.html', {'posts':posts})
+
+
+def upvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total += 1
+        post.save()
+        return redirect('home')
+
+
+def downvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total -= 1
+        post.save()
+        return redirect('home')
